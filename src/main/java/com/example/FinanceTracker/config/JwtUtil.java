e36cc255d258ce7c55cc0b531a6ef1a3aba9079a
prashantchaudhary001 <jaatprashant55@gmail.com>
2025-08-01 18:42:32 +0530

    Finance Tracker Web Application

--- src/main/java/com/example/FinanceTracker/config/JwtUtil.java
+++ src/main/java/com/example/FinanceTracker/config/JwtUtil.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.function.Function;
-
 @Component
 public class JwtUtil {
     
